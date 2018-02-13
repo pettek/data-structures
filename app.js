8b8a428c4ab805563aff2ddb98330039b4f0030d
Olbrot Piotr <POlbrot@hemmersbach.com>
2018-02-13 14:47:50 +0100

    Edit test file

--- app.js
+++ app.js
@@ -3,22 +3,17 @@ import Node from './src/list/node';
 
 let list = new List;
 list.add(new Node(5));
-list.add(new Node(10));
 list.add(new Node(3));
-list.add(new Node(null));
+list.add(10);
 list.add(106);
-list.remove(7)
-list.remove(-56)
+list.remove(7);
+list.remove(-56);
+let found = list.find(5);
 
 let emptyList = new List;
 emptyList.remove(new Node(-56));
 emptyList.find(new Node(2));
 
-let a = list.find(5);
-let b = list.find(3);
-let c = list.find(10);
-
 let string = list.toString();
 let array = list.toArray();
 
-console.log("");
